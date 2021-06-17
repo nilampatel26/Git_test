965166726de37e9a0cf14ea0d05e7ab3773a7706
nilampatel26 <nilam192patel@gmail.com>
2021-06-17 13:07:09 +0530

    add

---
+++ Caluculator.java
@@ -0,0 +1,13 @@
+import java.util*;
+public class Caluculator{
+public static void main (String [] args){
+int a,b;
+Scanner sc=new Scanner(System.in);
+a=sc.nextInt();
+b=sc.nextInt();
+int add = a+b;
+int diff=a-b;
+System.out.println(add);
+System.out.println(diff);
+}
+}
